c9a124d72a708f2aa68fbd336e213983b5bd8a92
readinggaol <dougparker.m@gmail.com>
2023-03-12 00:33:55 -0330

    Doug's updates
    A couple tasks

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+#imports are ways to include libraries of prewritten code
+#imports always go at the top 
+#in this case, I'm adding the 'random' library
+#I can then reference the libary by using its name
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#randint() is a method that generations a random integer
+#the two numbers are a range; it will generate between 0 and 10
+#it will not include 0, but will include 10
+myFirstRandomNumber = random.randint(0, 10)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#PUZZLE ONE
+#create a number-guessing game
+#have the computer generate a random number, then ask the user to guess
+#give the user three attempts to get the correct number
+#if the user gets it wrong, tell them whether they're too high or too low
+#if the user gets it right, don't ask them again
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#PUZZLE TWO
+#create a grade average calculator
+#get a name and three scores from the user
+#average the three scores and match it to a letter grade
+#assume the letter grades are: 0-49%: F, 50-59% D, 60-74% C, 75-84% B, 85-100% A
+#output should look like: Schmumu - A - 100
